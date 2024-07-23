325c7ff93c177dfc85f1ef6c0d1f83ff4369aa9f
David Soane <david.soane@outlook.com>
2024-07-22 19:45:33 -0600

    tried to fix socket listening error

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 #include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
 
-auto host = gethostbyname("");
-auto hostIP = inet_ntoa (*(struct in_addr *)*host->h_addr_list);
+int main() {    
+    SOCKET ListenSocket;
+    struct sockaddr_in saServer;
 
-int main() {
-    std::cout << hostIP;
+    auto host = gethostbyname("127.0.0.1");
+    auto hostIP = inet_ntoa (*(struct in_addr *)*host->h_addr_list);
+
+    std::cout << listen(ListenSocket, SOMAXCONN);
+
+    WSADATA wsaData;
+    int res = WSAStartup(MAKEWORD(2,2), &wsaData);
+    std::cout << ListenSocket << std::endl;
+
+    if ( listen( ListenSocket, SOMAXCONN ) == SOCKET_ERROR ) {
+        printf("Listen failed: %ld\n", WSAGetLastError() );
+        printf("Result from WSAStartup: %ld\n", res );
+        closesocket(ListenSocket);
+        WSACleanup();
+    };  
+
+
 };
+
+
+// to compile run:
+// g++ main.cpp -lws2_32
