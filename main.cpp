57c5be4f129589a070ea2564607249e9d6bf1a42
David Soane <david.soane@outlook.com>
2024-07-23 19:25:59 -0600

    added listening to the bound socket

--- main.cpp
+++ main.cpp
@@ -18,15 +18,21 @@ int main() {
   };
 
   SOCKET ServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
+  int port = 4000;
   sockaddr_in tcpaddr;
   tcpaddr.sin_family = AF_INET;
   tcpaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  tcpaddr.sin_port = htons("4000");
+  tcpaddr.sin_port = htons(port);
 
   bind(ServerSocket, (SOCKADDR *)&tcpaddr, sizeof(tcpaddr));
 
-  std::cout << "Socket successfully bound to tcp address"
+  std::cout << "Socket successfully bound to tcp address" << std::endl;
+
+  if ( listen(ServerSocket, 1) == SOCKET_ERROR ) {
+    std::cout << "An error occured when listening to this socket." << std::endl << WSAGetLastError() << std::endl;
+  } else {
+    std::cout << "Socket is listening with no errors";
+  }
 
 };
 
